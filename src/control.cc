364316a7ee0984ff38528bb345d75bb240d62fbb
Brian Taylor <brian.taylor@bolderflight.com>
2018-05-16 10:33:27 -0700

    Modified control to output data only for the engaged group AND level

--- src/control.cc
+++ src/control.cc
@@ -34,17 +34,21 @@ void ControlLaws::Configure(const rapidjson::Value& Config, DefinitionTree *Defi
       if (Config.HasMember(GroupName.GetString())) {
         // store the group key
         ResearchGroupKeys_.push_back(GroupName.GetString());
-        // path for the research functions /Control/"Group-Name"
-        std::string PathName = RootPath_+"/"+ResearchGroupKeys_.back();
+        // json group definition
         const rapidjson::Value& GroupDefinition = Config[GroupName.GetString()];
         // resize the control group by the number of levels
         ResearchControlGroups_[ResearchGroupKeys_.back()].resize(GroupDefinition.Size());
+        // resize the data keys by the number of levels
+        ResearchDataKeys_[ResearchGroupKeys_.back()].resize(GroupDefinition.Size());
         // iterate over the levels
         for (rapidjson::Value::ConstValueIterator Member = GroupDefinition.Begin(); Member != GroupDefinition.End(); ++Member) {
           if (Member->HasMember("Level")&&Member->HasMember("Components")) {
             auto level = std::distance(GroupDefinition.Begin(),Member);
             // store the level names
             ResearchLevelNames_[ResearchGroupKeys_.back()].push_back((*Member)["Level"].GetString());
+            // path for the research functions /Control/"Group-Name"
+            std::string PathName = RootPath_+"/"+ResearchGroupKeys_.back()+"/"+ResearchLevelNames_[ResearchGroupKeys_.back()].back();
+            // json components on a given level
             const rapidjson::Value& Components = (*Member)["Components"];
             // iterate over the components on each level
             for (auto &Func : Components.GetArray()) {
@@ -64,18 +68,18 @@ void ControlLaws::Configure(const rapidjson::Value& Config, DefinitionTree *Defi
                 throw std::runtime_error(std::string("ERROR")+PathName+std::string(": Type not specified in configuration."));
               }
             }
+            // getting a list of all research keys and adding to superset of output keys
+            // modify the key to remove the intermediate path 
+            // (i.e. /Control/GroupName/Pitch --> /Control/Pitch)
+            DefinitionTreePtr->GetKeys(PathName,&ResearchDataKeys_[ResearchGroupKeys_.back()][level]);
+            for (auto Key : ResearchDataKeys_[ResearchGroupKeys_.back()][level]) {
+              std::string MemberName = RootPath_+Key.substr(Key.rfind("/"));
+              if (Key.substr(Key.rfind("/"))!="/Mode") {
+                OutputKeysMap[MemberName] = MemberName;
+              }
+            }
           } else {
-            throw std::runtime_error(std::string("ERROR")+PathName+std::string(": Level name or components not specified in configuration."));
-          }
-        }
-        // getting a list of all research keys and adding to superset of output keys
-        // modify the key to remove the intermediate path 
-        // (i.e. /Control/GroupName/Pitch --> /Control/Pitch)
-        DefinitionTreePtr->GetKeys(PathName,&ResearchDataKeys_[ResearchGroupKeys_.back()]);
-        for (auto Key : ResearchDataKeys_[ResearchGroupKeys_.back()]) {
-          std::string MemberName = RootPath_+Key.substr(Key.rfind("/"));
-          if (Key.substr(Key.rfind("/"))!="/Mode") {
-            OutputKeysMap[MemberName] = MemberName;
+            throw std::runtime_error(std::string("ERROR")+RootPath_+std::string(": Level name or components not specified in configuration."));
           }
         }
       } else {
@@ -90,56 +94,60 @@ void ControlLaws::Configure(const rapidjson::Value& Config, DefinitionTree *Defi
     std::string OutputKey = OutputElem.second;
     // iterate through research keys
     for (auto GroupKey : ResearchGroupKeys_) {
-      for (auto ResearchKey : ResearchDataKeys_[GroupKey]) {
-        // check for a match with output keys
-        if (ResearchKey.substr(ResearchKey.rfind("/"))==OutputKey.substr(OutputKey.rfind("/"))) {
-          std::string KeyName = ResearchKey.substr(ResearchKey.rfind("/"));
-          // setup research data pointer
-          DefinitionTree::VariableDefinition TempDef;
-          DefinitionTreePtr->GetMember(ResearchKey,&TempDef);
-          ResearchDataPtr_[GroupKey][KeyName] = TempDef.Value;
-          // check to see if output key has already been registered
-          if (DefinitionTreePtr->Size(OutputKey)==0) {
-            // register output if it has not already been
-            if (DefinitionTreePtr->GetValuePtr<uint64_t*>(ResearchKey)) {
-              OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<uint64_t*>(ResearchKey));
-              DefinitionTreePtr->InitMember(OutputKey,std::get_if<uint64_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
-            }
-            if (DefinitionTreePtr->GetValuePtr<uint32_t*>(ResearchKey)) {
-              OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<uint32_t*>(ResearchKey));
-              DefinitionTreePtr->InitMember(OutputKey,std::get_if<uint32_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
-            }
-            if (DefinitionTreePtr->GetValuePtr<uint16_t*>(ResearchKey)) {
-              OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<uint16_t*>(ResearchKey));
-              DefinitionTreePtr->InitMember(OutputKey,std::get_if<uint16_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
-            }
-            if (DefinitionTreePtr->GetValuePtr<uint8_t*>(ResearchKey)) {
-              OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<uint8_t*>(ResearchKey));
-              DefinitionTreePtr->InitMember(OutputKey,std::get_if<uint8_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
-            }
-            if (DefinitionTreePtr->GetValuePtr<int64_t*>(ResearchKey)) {
-              OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<int64_t*>(ResearchKey));
-              DefinitionTreePtr->InitMember(OutputKey,std::get_if<int64_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
-            }
-            if (DefinitionTreePtr->GetValuePtr<int32_t*>(ResearchKey)) {
-              OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<int32_t*>(ResearchKey));
-              DefinitionTreePtr->InitMember(OutputKey,std::get_if<int32_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
-            }
-            if (DefinitionTreePtr->GetValuePtr<int16_t*>(ResearchKey)) {
-              OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<int16_t*>(ResearchKey));
-              DefinitionTreePtr->InitMember(OutputKey,std::get_if<int16_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
-            }
-            if (DefinitionTreePtr->GetValuePtr<int8_t*>(ResearchKey)) {
-              OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<int8_t*>(ResearchKey));
-              DefinitionTreePtr->InitMember(OutputKey,std::get_if<int8_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
-            }
-            if (DefinitionTreePtr->GetValuePtr<float*>(ResearchKey)) {
-              OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<float*>(ResearchKey));
-              DefinitionTreePtr->InitMember(OutputKey,std::get_if<float>(&OutputData_[KeyName]),TempDef.Description,true,false);
-            }
-            if (DefinitionTreePtr->GetValuePtr<double*>(ResearchKey)) {
-              OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<double*>(ResearchKey));
-              DefinitionTreePtr->InitMember(OutputKey,std::get_if<double>(&OutputData_[KeyName]),TempDef.Description,true,false);
+      // iterate through all levels
+      for (auto Levels = ResearchLevelNames_[GroupKey].begin(); Levels != ResearchLevelNames_[GroupKey].end(); ++Levels) {
+        auto Level = std::distance(ResearchLevelNames_[GroupKey].begin(),Levels);
+        for (auto ResearchKey : ResearchDataKeys_[GroupKey][Level]) {
+          // check for a match with output keys
+          if (ResearchKey.substr(ResearchKey.rfind("/"))==OutputKey.substr(OutputKey.rfind("/"))) {
+            std::string KeyName = ResearchKey.substr(ResearchKey.rfind("/"));
+            // setup research data pointer
+            DefinitionTree::VariableDefinition TempDef;
+            DefinitionTreePtr->GetMember(ResearchKey,&TempDef);
+            ResearchDataPtr_[GroupKey][KeyName] = TempDef.Value;
+            // check to see if output key has already been registered
+            if (DefinitionTreePtr->Size(OutputKey)==0) {
+              // register output if it has not already been
+              if (DefinitionTreePtr->GetValuePtr<uint64_t*>(ResearchKey)) {
+                OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<uint64_t*>(ResearchKey));
+                DefinitionTreePtr->InitMember(OutputKey,std::get_if<uint64_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
+              }
+              if (DefinitionTreePtr->GetValuePtr<uint32_t*>(ResearchKey)) {
+                OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<uint32_t*>(ResearchKey));
+                DefinitionTreePtr->InitMember(OutputKey,std::get_if<uint32_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
+              }
+              if (DefinitionTreePtr->GetValuePtr<uint16_t*>(ResearchKey)) {
+                OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<uint16_t*>(ResearchKey));
+                DefinitionTreePtr->InitMember(OutputKey,std::get_if<uint16_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
+              }
+              if (DefinitionTreePtr->GetValuePtr<uint8_t*>(ResearchKey)) {
+                OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<uint8_t*>(ResearchKey));
+                DefinitionTreePtr->InitMember(OutputKey,std::get_if<uint8_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
+              }
+              if (DefinitionTreePtr->GetValuePtr<int64_t*>(ResearchKey)) {
+                OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<int64_t*>(ResearchKey));
+                DefinitionTreePtr->InitMember(OutputKey,std::get_if<int64_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
+              }
+              if (DefinitionTreePtr->GetValuePtr<int32_t*>(ResearchKey)) {
+                OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<int32_t*>(ResearchKey));
+                DefinitionTreePtr->InitMember(OutputKey,std::get_if<int32_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
+              }
+              if (DefinitionTreePtr->GetValuePtr<int16_t*>(ResearchKey)) {
+                OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<int16_t*>(ResearchKey));
+                DefinitionTreePtr->InitMember(OutputKey,std::get_if<int16_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
+              }
+              if (DefinitionTreePtr->GetValuePtr<int8_t*>(ResearchKey)) {
+                OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<int8_t*>(ResearchKey));
+                DefinitionTreePtr->InitMember(OutputKey,std::get_if<int8_t>(&OutputData_[KeyName]),TempDef.Description,true,false);
+              }
+              if (DefinitionTreePtr->GetValuePtr<float*>(ResearchKey)) {
+                OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<float*>(ResearchKey));
+                DefinitionTreePtr->InitMember(OutputKey,std::get_if<float>(&OutputData_[KeyName]),TempDef.Description,true,false);
+              }
+              if (DefinitionTreePtr->GetValuePtr<double*>(ResearchKey)) {
+                OutputData_[KeyName] = *(DefinitionTreePtr->GetValuePtr<double*>(ResearchKey));
+                DefinitionTreePtr->InitMember(OutputKey,std::get_if<double>(&OutputData_[KeyName]),TempDef.Description,true,false);
+              }
             }
           }
         }
@@ -190,7 +198,7 @@ void ControlLaws::RunEngaged(size_t ControlLevel) {
       Func->Run(GenericFunction::kEngage);
     }
     // output research control laws
-    for (auto Key : ResearchDataKeys_[EngagedGroup_]) {
+    for (auto Key : ResearchDataKeys_[EngagedGroup_][ControlLevel]) {
       std::string KeyName = Key.substr(Key.rfind("/"));
       if (KeyName!="/Mode") {
         if (std::get_if<uint64_t*>(&ResearchDataPtr_[EngagedGroup_][KeyName])) {
--- src/control.hxx
+++ src/control.hxx
@@ -105,7 +105,7 @@ class ControlLaws {
     std::map<std::string,std::vector<std::vector<std::shared_ptr<GenericFunction>>>> ResearchControlGroups_;
     std::vector<std::string> ResearchGroupKeys_;
     std::map<std::string,std::vector<std::string>> ResearchLevelNames_;
-    std::map<std::string,std::vector<std::string>> ResearchDataKeys_;
+    std::map<std::string,std::vector<std::vector<std::string>>> ResearchDataKeys_;
     std::map<std::string,std::variant<uint64_t,uint32_t,uint16_t,uint8_t,int64_t,int32_t,int16_t,int8_t,float, double>> OutputData_;
     std::map<std::string,std::map<std::string,std::variant<uint64_t*,uint32_t*,uint16_t*,uint8_t*,int64_t*,int32_t*,int16_t*,int8_t*,float*,double*>>> ResearchDataPtr_;
 };
